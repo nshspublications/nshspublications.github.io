2ab50c634aa29b6ccd92a5a4796a10ce4ac026a0
NSHS <nshscentralholding17@gmail.com>
2026-05-15 15:31:36 -0500

    Add files via upload

---
+++ atkinsen/baretests/test1/wakelock.js
@@ -0,0 +1,26 @@
+let wakeLock = null;
+
+const requestWakeLock = async () => {
+  try {
+    wakeLock = await navigator.wakeLock.request('screen');
+    console.log('Wake Lock is active!');
+  } catch (err) {
+    // The request can fail if the device is in low-power mode 
+    // or if the window is not visible.
+    console.error(`${err.name}, ${err.message}`);
+  }
+};
+
+document.addEventListener('visibilitychange', async () => {
+  if (wakeLock !== null && document.visibilityState === 'visible') {
+    await requestWakeLock();
+  }
+});
+
+function ReleaseWakeLock(){
+    if (wakeLock) {
+  wakeLock.release().then(() => {
+    wakeLock = null;
+  });
+}
+}
