7ddd7784823dd52ba7b9628d3c2805ee3db68b88
NSHS <nshscentralholding17@gmail.com>
2026-05-15 18:01:10 -0500

    Update main.js

--- atkinsen/baretests/test1/main.js
+++ atkinsen/baretests/test1/main.js
@@ -1,8 +1,34 @@
 var COE = document.getElementById('dbgconout');
+var CONALL = "";
 
 function dbg_cout(s){
-    console.log(s);
+    //console.log(s);
     COE.textContent+=s;
+    CONALL+=s;
 }
 
-setInterval(()=>{dbg_cout("Test ");requestWakeLock()},500)
+function dbg_clearcon(){
+    COE.textContent="";
+}
+
+//setInterval(()=>{dbg_cout("Test ");requestWakeLock()},500)
+
+setInterval(()=>{
+    //
+    dbg_clearcon();
+    dbg_cout("; Wake Lock status: "+JSON.stringify(wakeLock)+"\n"
+    +"");
+},1000/15);
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js").then(
+    (registration) => {
+      console.log("Service worker registration successful:", registration);
+    },
+    (error) => {
+      console.error(`Service worker registration failed: ${error}`);
+    },
+  );
+} else {
+  console.error("Service workers are not supported.");
+}
