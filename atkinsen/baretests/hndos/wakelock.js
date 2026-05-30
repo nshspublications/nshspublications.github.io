ac16217b8705794ead92929ca0f517e94f058e54
NSHS <nshscentralholding17@gmail.com>
2026-05-30 02:57:08 -0500

    Add files via upload

--- atkinsen/baretests/hndos/wakelock.js
+++ atkinsen/baretests/hndos/wakelock.js
@@ -3,12 +3,12 @@ class WakeLock {
   static requestWakeLock = async () => {
     try {
       WakeLock.wakeLockObject = await navigator.wakeLock.request('screen');
-      Console.write('\nWake Lock is active!\n');
+      Console.write('[wakelock.js] Wake Lock is active!\n');
     } catch (err) {
       // The request can fail if the device is in low-power mode 
       // or if the window is not visible.
       console.error(`${err.name}, ${err.message}`);
-      Console.write("\n"+'ERROR: '+`${err.name}, ${err.message}`+"\n");
+      Console.write(""+'ERROR: '+`${err.name}, ${err.message}`+"\n");
     }
   };
   static ReleaseWakeLock(){
@@ -20,10 +20,22 @@ class WakeLock {
   }
 }
 
-Console.write("wakelock.js reached\n");
+Console.write("[wakelock.js] reached\n");
+
+WakeLock.requestWakeLock(); //needs to happen once to initialize WakeLock.wakeLockObject
 
 document.addEventListener('visibilitychange', async () => {
+  Console.write("[wakelock.js] visibilitychange triggered\n");
   if (WakeLock.wakeLockObject !== null && document.visibilityState === 'visible') {
+    Console.write("[wakelock.js] visibilityState: visible\n");
+    await WakeLock.requestWakeLock();
+  }
+});
+
+document.addEventListener('click', async () => {
+  Console.write("[wakelock.js] click triggered\n");
+  if (WakeLock.wakeLockObject !== null && document.visibilityState === 'visible') {
+    Console.write("[wakelock.js] visibilityState: visible\n");
     await WakeLock.requestWakeLock();
   }
 });
