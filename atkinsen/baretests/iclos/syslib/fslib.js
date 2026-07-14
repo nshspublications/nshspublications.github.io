4a940aadbcb7eb86e3a9190da8995cbf92ccce0a
NSHS <nshscentralholding17@gmail.com>
2026-07-14 01:15:43 -0500

    Add files via upload

---
+++ atkinsen/baretests/iclos/syslib/fslib.js
@@ -0,0 +1,61 @@
+/*class fslib {
+    static DBFS = class { //simple entry fs, no directories
+        constructor(){
+            this.drive = [];
+        }
+    }
+    static ITreeFS = class { //more complex/realistic fs, tree node structure
+        constructor(){
+            this.drive = [];
+        }
+    }
+}*/
+
+class StdPOSIXRouter {
+    constructor(){
+
+    }
+    open(){
+
+    }
+    read(){
+
+    }
+    write(){
+
+    }
+    close(){
+
+    }
+    ioctl(){
+
+    }
+    fcntl(){
+
+    }
+    select(){
+
+    }
+    poll(){
+
+    }
+    tcgetattr(){
+
+    }
+    tcsetattr(){
+        
+    }
+}
+
+class DeviceDriverWrappers {
+    static StorageDevice = class {
+        constructor(){
+
+        }
+    }
+    static HumanInterfaceDevice = class {
+        constructor(){
+
+        }
+    }
+}
